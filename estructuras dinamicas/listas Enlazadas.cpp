ec2408d32f6ecc9066d2a6854229c9581e68dc66
Agus Bes7 <aguscabj10.ab@gmail.com>
2025-09-23 19:29:58 -0300

    practica de estructuras dinamicas 2 listas enlazadas y vectores y matrices dinamicas

---
+++ estructuras dinamicas/listas Enlazadas.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+using namespace std;
+//USAREMOS UNA ESTRUCTURA SIMPLE PARA PONER A PRUEBA TODAS LAS OPERACIONES
+#define tam 20
+struct Nodo{
+	int dato;
+	Nodo * sig;
+};
+typedef Nodo *ptr;
+
+void agregarInicio(ptr * L,int valor);
+void agregarFinal(ptr * L,int valor);
+void insercionOrdenada(ptr * L,int valor);
+void mostrarTodos(ptr L);
+void borraFinal(ptr *L);
+void borraInicio(ptr *L);
+int menor(ptr L);
+void imprimirInverso(ptr L);
+void borrarL(ptr *L);
+
+//---------------------------------------------
+int main(int argc, char *argv[]) {
+	ptr L=NULL, aux,nuevo;
+	agregarInicio(&L,8);
+	insercionOrdenada(&L,6);
+	insercionOrdenada(&L,65);
+	insercionOrdenada(&L,4);
+	insercionOrdenada(&L,25);
+	insercionOrdenada(&L,32);
+/*	agregarInicio(&L,5);
+	agregarFinal(&L,6);
+	agregarInicio(&L,3);
+	agregarInicio(&L,2);
+	agregarInicio(&L,1);
+	agregarFinal(&L,7);
+	agregarFinal(&L,8);
+	agregarInicio(&L,4);
+	insercionOrdenada(&L,3);
+	borraFinal(&L);*/
+
+	mostrarTodos(L);
+	cout<<"menor: "<<menor(L)<<endl;
+/*imprimirInverso(L);
+borrarL(&L);
+mostrarTodos(L);*/
+	return 0;
+}
+//---------------------------------------------
+void agregarInicio(ptr * L,int valor){
+	ptr aux=*L;
+	(*L)=new Nodo;
+	if((*L)==NULL){
+		cout<<"Error..."<<endl;}
+	else{
+	(*L)->dato=valor;
+	(*L)->sig= aux;
+	}}
+	//---------------------------------------------	
+void agregarFinal(ptr *L,int valor){
+	ptr aux=*L;
+	while(aux->sig!=NULL){
+		aux=aux->sig;}
+	ptr nuevo=new Nodo;
+	if(nuevo!=NULL){
+		nuevo->dato=valor;
+		nuevo->sig=NULL;
+		aux->sig=nuevo;
+	}
+	else{cout<<"error...";}}
+	
+//---------------------------------------------
+void borraInicio(ptr *L){
+ptr aux=(*L)->sig;
+delete *L;
+(*L)=aux;}
+//---------------------------------------------
+void borraFinal(ptr *L){
+ptr aux=*L,anterior;
+while(aux->sig!=NULL){
+anterior=aux;			aux=aux->sig;}
+anterior->sig=NULL;
+delete aux;}
+//---------------------------------------------	
+void mostrarTodos(ptr L){
+	if(L==NULL){cout<<"lista vacia..."<<endl; return ;}
+	while(L!=NULL){
+		cout<<L->dato<<" - ";
+		L=L->sig;	}
+cout<<endl;}
+//---------------------------------------------
+int menor(ptr L){
+	int menor=L->dato;
+	ptr aux=L->sig;
+	while(aux!=NULL){
+		if(menor>aux->dato){menor=aux->dato;}
+		aux=aux->sig;
+	}
+return menor;}
+	
+
+//-----------------------------------
+	void imprimirInverso(ptr L){
+		
+	if(L->sig==NULL){ cout<<L->dato<<"-";}
+	else{
+		imprimirInverso(L->sig);
+		cout<<L->dato<<"-";
+	}}
+
+void borrarL(ptr *L){
+	ptr aux=*L;
+	while(aux!=NULL){
+		aux=(*L)->sig;
+		delete *L;
+		*L=aux;
+	}
+}
+void insercionOrdenada(ptr *L,int valor){
+	ptr aux=*L;
+	ptr anterior=aux,tmpr;
+	if(aux->dato>=valor){
+	agregarInicio(&(*L),valor);
+		}
+	
+
+	int insercion=-1;
+	aux=aux->sig;
+	while(aux!=NULL  and insercion==-1){
+		if(aux->dato>=valor){
+			insercion=1;
+			tmpr=new Nodo;
+			tmpr->dato=valor;
+			anterior->sig=tmpr;
+			tmpr->sig=aux;
+			
+		}
+		else{
+			anterior=aux;
+			aux=aux->sig;
+		}
+	}
+	if(aux==NULL){
+		tmpr=new Nodo;
+		tmpr->dato=valor;
+		anterior->sig=tmpr;
+		tmpr->sig=NULL;
+	}
+
+}
+		
